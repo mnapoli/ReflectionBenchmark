b5ae12f5dcf80bd1d3cdb04c683d293f2196527f
Matthieu Napoli <matthieu@mnapoli.fr>
2013-08-18 09:49:50 +0200

    Write property benchmark

---
+++ benchmarks/Benchmark/WritePropertyEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Benchmark;
+
+use Athletic\AthleticEvent;
+use Benchmark\Fixture\Foo;
+use Closure;
+
+class WritePropertyEvent extends AthleticEvent
+{
+    private $object;
+    private $propertyName;
+    private $closure;
+
+    public function setUp()
+    {
+        $this->object = new Foo('test');
+        $this->propertyName = 'prop';
+        $this->closure = function($prop, $value) {
+            $this->{$prop} = $value;
+        };
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function reflection()
+    {
+        $reflectionProperty = new \ReflectionProperty($this->object, $this->propertyName);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($this->object, 'test2');
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function closure()
+    {
+        $closure = Closure::bind($this->closure, $this->object, $this->object);
+        $closure($this->propertyName, 'test2');
+    }
+}
