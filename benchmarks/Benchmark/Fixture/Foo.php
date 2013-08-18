64bba5af38b064161e1875683c2f11d3c8c782d1
Matthieu Napoli <matthieu@mnapoli.fr>
2013-08-18 09:46:55 +0200

    Read property benchmark

---
+++ benchmarks/Benchmark/Fixture/Foo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Benchmark\Fixture;
+
+class Foo
+{
+    private $prop;
+
+    public function __construct($prop)
+    {
+        $this->prop = $prop;
+    }
+}
---
+++ benchmarks/Benchmark/ReadPropertyEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Benchmark;
+
+use Athletic\AthleticEvent;
+use Benchmark\Fixture\Foo;
+use Closure;
+
+class ReadPropertyEvent extends AthleticEvent
+{
+    private $object;
+    private $propertyName;
+    private $closure;
+
+    public function setUp()
+    {
+        $this->object = new Foo('test');
+        $this->propertyName = 'prop';
+        $this->closure = function($prop) {
+            return $this->{$prop};
+        };
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function reflection()
+    {
+        $reflectionProperty = new \ReflectionProperty($this->object, $this->propertyName);
+        $reflectionProperty->setAccessible(true);
+        $value = $reflectionProperty->getValue($this->object);
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function arrayCast()
+    {
+        $array = (array) $this->object;
+        $value = $array["\0" . get_class($this->object) . "\0" . $this->propertyName];
+    }
+
+    /**
+     * @iterations 10000
+     */
+    public function closure()
+    {
+        $closure = Closure::bind($this->closure, $this->object, $this->object);
+        $value = $closure($this->propertyName);
+    }
+}
